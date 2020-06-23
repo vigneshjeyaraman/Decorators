ed994bb4946fe437e0a6cfcc6e79637246680840
vigneshjeyaraman <j.vignesh2794@outlook.com>
2020-06-23 18:07:07 +0530

    first simple decorator.

--- example1/example1.py
+++ example1/example1.py
@@ -1,3 +1,14 @@
-def hello():
-    print("hello")
-hello()
+
+def simple_decorator(func):
+    def wrapper():
+        print("Hey I am decorating you buddy")
+        func()
+        print("I decorated you.")
+    return wrapper
+
+@simple_decorator
+def decorate_me():
+    print("Someone is going to get call before me :P")
+
+
+decorate_me()
