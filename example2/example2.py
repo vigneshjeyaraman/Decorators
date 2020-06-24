25cc164335d5430c809411429a29644f49ab74d8
vigneshjeyaraman <j.vignesh2794@outlook.com>
2020-06-24 08:36:12 +0530

    changed print message

--- example2/example2.py
+++ example2/example2.py
@@ -1,13 +1,13 @@
 def simple_decorator(func):
     def wrapper2(*args, **kwargs):
-        print("Hey I am decorating you buddy")
+        print("Hey, I am decorating you buddy")
         func(*args, *kwargs)
         print("I decorated you.")
     return wrapper2
 
 @simple_decorator
 def decorate_me(arg1, arg2):
-    print(f"Hey I have received two argruments {arg1 !r} and {arg2 !r}")
+    print(f"Hey, I have received two argruments {arg1 !r} and {arg2 !r}")
 
 
 decorate_me("one", "two")
