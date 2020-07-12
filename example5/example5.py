6c3c29efa6034d89c6b4755650f6ebbb06a20f40
= <j.vignesh2794@outlook.com>
2020-07-12 13:15:12 +0530

    added argument to decorator

---
+++ example5/example5.py
@@ -0,0 +1,20 @@
+import functools
+
+
+def simple_decorator(decorator_arg):
+    def additional_wrapper(func):
+        @functools.wraps(func)
+        def wrapper4(*args, **kwargs):
+            print(f"Hey, I am decorator printing and " \
+                  f"this time decorator also got argument {decorator_arg}")
+            func(*args, **kwargs)
+        return wrapper4
+    return additional_wrapper
+
+@simple_decorator(33)
+def decorate_me(arg1, arg2):
+    print(f"Hey, I am the original function with following arguments {arg1}, {arg2}")
+
+a = decorate_me
+a(2,4)
+print(a.__name__)
