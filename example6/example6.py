b071c787377c408698d16d1afdb2a21d6ec1f4dd
= <j.vignesh2794@outlook.com>
2020-07-19 13:25:22 +0530

    added cache example

---
+++ example6/example6.py
@@ -0,0 +1,30 @@
+import functools
+
+
+def count_calls(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        print(f"Function is called {wrapper.count} times")
+        return func(*args, **kwargs)
+    wrapper.count = 0
+    return wrapper
+
+def cache(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args not in wrapper.cache:
+            wrapper.cache[args] = func(*args, **kwargs)
+        return wrapper.cache[args]
+    
+    wrapper.cache = dict()
+    return wrapper
+
+@cache    
+@count_calls
+def fib(num):
+    if num < 2:
+        return num
+    return fib(num-1) + fib(num-2)
+
+fib(12)
